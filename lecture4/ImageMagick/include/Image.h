18367a420ba7baf904292c75dcafaefaf169ebac
demiaster <marta.celeste.feriani@gmail.com>
2016-10-27 21:44:18 +0200

    new project: ImageMagick

---
+++ lecture4/ImageMagick/include/Image.h
@@ -0,0 +1,40 @@
+#ifndef IMAGE_H_
+#define IMAGE_H_
+#include <cstddef>
+#include <string>
+#include <memory>
+
+class Image
+{
+    public:
+        inline Image(std::size_t _width,
+                     std::size_t _height,
+                     std::size_t _depth = 3) :
+            m_width(_width),
+            m_height(_height),
+            m_pixelDepth(_depth),
+            m_data(new unsigned char[m_width*m_height*m_pixelDepth]) {;}
+
+        void setPixel(std::size_t _x,
+                      std::size_t _y,
+                      unsigned char _r,
+                      unsigned char _g,
+                      unsigned char _b);
+        bool save(std::string _fname);
+
+        void clearScreen(unsigned char _r,
+                         unsigned char _g,
+                         unsigned char _b);
+
+
+
+    private:
+        std::unique_ptr<unsigned char> m_data;
+        std::size_t m_height;
+        std::size_t m_width;
+        std::size_t m_pixelDepth;
+
+
+};
+
+#endif
---
+++ lecture4/ImageMagick/src/main.cpp
@@ -0,0 +1,8 @@
+#include "Image.h"
+#include <cstdlib>
+#include <iostream>
+int main()
+{
+  std::cout<<"Need to write an Image Class\n";
+  return EXIT_SUCCESS;
+}
