dbaa0b8b64f3f4c9ab1330acf8ab6fdad0efc4ce
demiaster <marta.celeste.feriani@gmail.com>
2016-10-28 16:04:05 +0000

    Image.h: documentation added

--- lecture4/ImageMagick/include/Image.h
+++ lecture4/ImageMagick/include/Image.h
@@ -7,6 +7,13 @@
 class Image
 {
     public:
+
+        /// @brief constructor passing in width, height of the image
+        /// and the depth of the color
+        /// @param[in] _width width of the image
+        /// @param[in] _height height of the image
+        /// @param[in] _depth number of color component
+
         inline Image(std::size_t _width,
                      std::size_t _height,
                      std::size_t _depth = 3) :
@@ -15,22 +22,42 @@ class Image
             m_pixelDepth(_depth),
             m_data(new unsigned char[_width * _height * _depth]) {;}
 
+        /// mutators
+        /// @brief set the specified pixel to given r, g, b values
+        /// @param[in] _x x index for pixel position
+        /// @param[in] _y y index for pixel position
+        /// @param[in] _r red component
+        /// @param[in] _g green component
+        /// @param[in] _b blue component
+
         void setPixel(std::size_t _x,
                       std::size_t _y,
                       unsigned char _r,
                       unsigned char _g,
-                      unsigned char _b);
-        bool save(std::string _fname);
+                      unsigned char _b);        
+
+        /// @brief set the whole image color according to given r, g, b values
+        /// @param[in] _r red component
+        /// @param[in] _g green component
+        /// @param[in] _b blue component
 
         void clearScreen(unsigned char _r,
                          unsigned char _g,
                          unsigned char _b);
+
+        /// @brief save image to output file
+        /// @param[in] _fname name of the output file
+
+        bool save(std::string _fname);
+
+        /// @brief destructor
 
         ~Image();
 
 
 
     private:
+
         std::unique_ptr<unsigned char[]> m_data;
         std::size_t m_height;
         std::size_t m_width;
