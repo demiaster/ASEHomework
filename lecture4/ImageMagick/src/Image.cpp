ca977639b78bf1797e77934e2cbc5f89fd06e596
demiaster <marta.celeste.feriani@gmail.com>
2016-10-28 16:44:00 +0000

    Image.cpp: implemented clearScreen function

--- lecture4/ImageMagick/src/Image.cpp
+++ lecture4/ImageMagick/src/Image.cpp
@@ -16,11 +16,24 @@ void Image::setPixel(std::size_t _x,
     m_data[getIndex(_x, _y, 0)] = _r;
     m_data[getIndex(_x, _y, 1)] = _g;
     m_data[getIndex(_x, _y, 2)] = _b;
+
+    return;
 }
 
 bool Image::save(std::string _fname) {}
 
 void Image::clearScreen(unsigned char _r,
                  unsigned char _g,
-                 unsigned char _b) {}
+                 unsigned char _b)
+{
+    for( int i = 0; i < m_width; ++i)
+    {
+        for( int j = 0; j < m_height; ++j)
+        {
+            setPixel(i, j, _r, _g, _b);
+        }
+    }
+
+    return;
+}
 
